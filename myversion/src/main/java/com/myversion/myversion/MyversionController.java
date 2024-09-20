7b41226f47e1b615ee89dd2aed9aad8b877d1e36
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-09-21 01:15:58 +0900

    check_api_comm

--- myversion/src/main/java/com/myversion/myversion/MyversionController.java
+++ myversion/src/main/java/com/myversion/myversion/MyversionController.java
@@ -3,6 +3,9 @@ package com.myversion.myversion;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 @RequestMapping("/myversion")
@@ -24,6 +27,29 @@ public class MyversionController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+    }
+    
 
     @GetMapping("/upload")
     public String getFlaskUrl() {
