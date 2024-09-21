d1eed5ffbecb44b833ddaad4f95ab5999eabe35c
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-09-21 20:27:54 +0900

    Spring Data JPA 연결

---
+++ myversion/src/main/java/com/myversion/myversion/SpringConfig.java
@@ -0,0 +1,24 @@
+package com.myversion.myversion;
+
+import com.myversion.myversion.repository.SongRepository;
+import com.myversion.myversion.service.SongService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig{
+    private final SongRepository songRepository;
+
+    @Autowired
+    public SpringConfig(SongRepository songRepository) {
+        this.songRepository = songRepository;
+    }
+
+    @Bean
+    public SongService songService(){
+        return new SongService(songRepository);
+    }
+}
