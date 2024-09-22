f813672d1f4a5de595bd1826d26ae482965f4c0e
이송헌 <45353929+LightlessStar@users.noreply.github.com>
2024-09-22 21:00:50 +0900

    flask 기초
    flask

---
+++ myversion/src/main/java/com/myversion/myversion/controller/FlaskController.java
@@ -0,0 +1,20 @@
+package com.myversion.myversion.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotation.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "FlaskController", description = "Flas 관련 기능 컨트롤러")
+public class FlaskController {
+    private final FlaskService flaskService;
+
+    @PostMapping("/flask")
+    public String sendToFlask(@RequestBody RequestSendToFlaskDto dto) throws JsonProcessingException {
+        return flaskService.sendToFlask(dto);
+    }
+}
