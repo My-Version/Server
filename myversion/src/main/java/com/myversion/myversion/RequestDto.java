430825b86a24a945b96cef8ad3da8a0416c48582
monic <song001209@naver.com>
2024-09-23 16:12:53 +0900

    flask Removal
    flask 제거 작업

--- myversion/src/main/java/com/myversion/myversion/RequestDto.java
+++
@@ -1,9 +0,0 @@
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
-public class RequestSendToFlaskDto {
-    private final String name;
-    private final String fileId;
-}
