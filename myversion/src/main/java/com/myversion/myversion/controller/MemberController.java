eccd031cfc0d3acafcaaa68a6011364e1d22982d
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-10-26 00:17:05 +0900

    feat(controller) : add MemberController
    1. MemberController
    SpringDataJPA를 사용, API 형태로 Member CRUD 기능을 노출

---
+++ myversion/src/main/java/com/myversion/myversion/controller/MemberController.java
@@ -0,0 +1,47 @@
+package com.myversion.myversion.controller;
+
+import com.myversion.myversion.domain.Member;
+import com.myversion.myversion.service.MemberService;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/members")
+public class MemberController {
+    private final MemberService memberService;
+
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    // Create or Update
+    @PostMapping
+    public Member createMember(@RequestBody Member member) {
+        return memberService.saveMember(member);
+    }
+
+    // Read by ID
+    @GetMapping("/{id}")
+    public Optional<Member> getMember(@PathVariable Long id) {
+        return memberService.findMemberById(id);
+    }
+
+    // Read All
+    @GetMapping
+    public List<Member> getAllMembers() {
+        return memberService.findAllMembers();
+    }
+
+    // Delete by ID
+    @DeleteMapping("/{id}")
+    public void deleteMember(@PathVariable Long id) {
+        memberService.deleteMemberById(id);
+    }
+}
