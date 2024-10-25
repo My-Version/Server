9a1c0f3f8c6951e77fc278973a76634a2bae3f73
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-10-26 00:04:17 +0900

    feat(repository) : create MemberRepository interface
    Member 관리를 위한 기본 리포지토리 생성
     - SpringDataJPA 리포지토리를 사용하지만 확장성을 위해 기본 리포지토리를 구현

---
+++ myversion/src/main/java/com/myversion/myversion/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.myversion.myversion.repository;
+
+import com.myversion.myversion.domain.Member;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(int id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
