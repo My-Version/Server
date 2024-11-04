1cde6d81f26fbd4e7c24c2e77814aa0b24d9b083
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-11-04 23:14:10 +0900

    feat(service) : add login feature

--- myversion/src/main/java/com/myversion/myversion/service/MemberService.java
+++ myversion/src/main/java/com/myversion/myversion/service/MemberService.java
@@ -1,18 +1,21 @@
 package com.myversion.myversion.service;
 
 import com.myversion.myversion.domain.Member;
+import com.myversion.myversion.repository.MemberRepository;
 import com.myversion.myversion.repository.MemberSpringDataJpaRepository;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
-    private final MemberSpringDataJpaRepository memberRepository;
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public MemberService(MemberSpringDataJpaRepository memberRepository) {
+    public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
@@ -20,6 +23,15 @@ public class MemberService {
     public Member saveMember(Member member) {
         return memberRepository.save(member);
     }
+
+    public Member login(Long id, String password) {
+        Optional<Member> member = memberRepository.findById(id);
+        if (member.isPresent() && Objects.equals(member.get().getPassWord(), password)) {
+            return member.get();
+        }
+        return null;
+    }
+
 
     // Find by ID
     public Optional<Member> findMemberById(Long id) {
