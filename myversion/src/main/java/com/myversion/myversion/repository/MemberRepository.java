b41ee5b0dd745741bb996aa35e12e0a0ba4097c6
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-11-05 22:31:03 +0900

    fix(repository) : id type change

--- myversion/src/main/java/com/myversion/myversion/repository/MemberRepository.java
+++ myversion/src/main/java/com/myversion/myversion/repository/MemberRepository.java
@@ -6,8 +6,8 @@ import java.util.Optional;
 
 public interface MemberRepository {
     Member save(Member member);
-    Optional<Member> findById(Long id);
+    Optional<Member> findById(String id);
     Optional<Member> findByName(String name);
     List<Member> findAll();
-    void deleteById(Long id);
+    void deleteById(String id);
 }
