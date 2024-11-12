bedddc42aab05b42a834e4c82af298ef8102d2f1
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-11-13 01:11:49 +0900

    fix(controller) : CompareController
    - 결과 링크 String 탐색 방법 수정
    - 기능 추가)
     - compareResult
     - compareImage
     - compareList

--- myversion/src/main/java/com/myversion/myversion/controller/CompareController.java
+++ myversion/src/main/java/com/myversion/myversion/controller/CompareController.java
@@ -1,26 +1,19 @@
 package com.myversion.myversion.controller;
 
 
+import com.myversion.myversion.domain.Compare;
 import com.myversion.myversion.service.CoverSongService;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.myversion.myversion.service.CompareService;
@@ -57,42 +50,39 @@ public class CompareController {
 
         // 비동기 비교 작업 시작
         CompletableFuture<List<String>> futureResult = CompareService.compareSongAsync(recordUrl, coverSongUrl);
-        // 작업 완료 후 실행할 동작 정의
+        // 작업 완료 후 실행할 동작
         futureResult.thenAccept(result -> {
-            // 2. id로 CompareDB 에 json이랑 img파일 링크 저장
-            String jsonFileLocation = result.get(2);
-            String pngFileLocation = result.get(3);
+            String jsonFileLocation = result.stream()
+                    .filter(item -> item.startsWith("https:") && item.endsWith(".json"))
+                    .findFirst()
+                    .orElse(null);
+
+            // https:로 시작하고 .png로 끝나는 문자열을 필터링하여 찾기
+            String pngFileLocation = result.stream()
+                    .filter(item -> item.startsWith("https:") && item.endsWith(".png"))
+                    .findFirst()
+                    .orElse(null);
             compareService.updateCompareFileLocations(compareId, jsonFileLocation, pngFileLocation);
         });
         return ResponseEntity.ok(compareId);
     }
 
-
-    // compare DB에서 생성된 이미지 파일의 S3위치를 읽어서 JSON 다운로드해서 넘김
+    // compare DB에서 Json 파일의 S3 링크를 넘김
     @GetMapping("/compareResult")
-    public ResponseEntity<?> compareResult(@RequestParam String compareResultDir) throws IOException {
-        String jsonData = "{\"userDir\":\"Hello, World!\"}";
-        HttpHeaders jsonHeaders = new HttpHeaders();
-        jsonHeaders.setContentType(MediaType.APPLICATION_JSON);
-
-        return ResponseEntity.ok()
-                .headers(jsonHeaders)
-                .body(jsonData);
+    public String compareResult(@RequestParam Long compareId) throws IOException {
+        return compareService.getCompareResultS3Path(compareId, true);
     }
 
-    // compare DB에서 생성된 이미지 파일의 S3위치를 읽어서 바이트코드로 넘김
+    // compare DB에서 이미지 파일의 S3 링크를 넘김
     @GetMapping("/compareImage")
-    public ResponseEntity<?> compareImage(@RequestParam Long compareId) throws IOException {
-        String imgS3Path = compareService.getCompareResultS3Path(compareId, false);  // 여기에 링크
-        byte[] imageBytes = Files.readAllBytes(Paths.get(imgS3Path));
-        InputStreamResource imageResource = new InputStreamResource(new ByteArrayInputStream(imageBytes));
+    public String compareImage(@RequestParam Long compareId) throws IOException {
+        return compareService.getCompareResultS3Path(compareId, false);
+    }
 
-        HttpHeaders imageHeaders = new HttpHeaders();
-        imageHeaders.setContentType(MediaType.IMAGE_PNG);
-
-        return ResponseEntity.ok()
-                .headers(imageHeaders)
-                .body(imageResource);
+    // userId의 compareList
+    @GetMapping("/compareList")
+    public List<Compare> compareList(@RequestParam String userId) throws IOException {
+        return compareService.getCompareListByUserId(userId);
     }
 
     private ResponseEntity<?> recordUpload(MultipartFile file) throws IOException {
