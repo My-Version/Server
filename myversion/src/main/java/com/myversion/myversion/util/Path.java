206a95d37c75e10a8a51796fb4799ba740e9f579
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-11-13 01:15:00 +0900

    feat(util) : Path
    - python 파일 경로 상수화

---
+++ myversion/src/main/java/com/myversion/myversion/util/Path.java
@@ -0,0 +1,7 @@
+package com.myversion.myversion.util;
+
+public class Path {
+    public static final String compareFilePath_EC2 = "/home/ec2-user/tone_compare.py";
+    public static final String compareFilePath_local = "C:/Users/juyon/python/myversion/tone_compare.py";
+
+}
